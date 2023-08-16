c349e89830b130a27de0ba30e21fe55d79c52567
huangdawei@shizhuang-inc.com <huangdawei@shizhuang-inc.com>
2023-08-17 00:28:00 +0800

    update to json log

--- lib/logger/logrus.go
+++ lib/logger/logrus.go
@@ -68,7 +68,7 @@ func (f *UpperCaseJSONFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 	newData["details"] = details
 
 	// 将更改后的map重新编码为JSON
-	jsonBytes, err := json.Marshal(data)
+	jsonBytes, err := json.Marshal(newData)
 	if err != nil {
 		return nil, err
 	}
